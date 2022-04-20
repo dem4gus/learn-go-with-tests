c98703cf1c1837e7ab6614598cd451f1f96539c4
Jason Healy <jason@jhealy.net>
2022-04-20 04:52:06 +0000

    implement hello

---
+++ hello/hello_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	want := "Hello, Chris"
+
+	if got := Hello("Chris"); got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
