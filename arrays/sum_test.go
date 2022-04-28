961da17b269b04573ba7a26c3a96dff35077c200
Jason Healy <jason@jhealy.net>
2022-04-28 04:52:13 +0000

    finish arrays

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -23,3 +23,23 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("expected %v but got %v", want, got)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("expected %v but got %v", want, got)
+		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely run empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+}
