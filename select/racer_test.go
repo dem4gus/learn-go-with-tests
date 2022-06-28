bfb01557db24e79d99fb41cee1056d0dd6a0bcdc
Jason Healy <jason@jhealy.net>
2022-06-28 06:45:09 +0000

    use helper func in test

--- select/racer_test.go
+++ select/racer_test.go
@@ -8,21 +8,25 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(20 * time.Millisecond)
-		w.WriteHeader(http.StatusOK)
-	}))
-	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	}))
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
 	slowUrl := slowServer.URL
 	fastUrl := fastServer.URL
 
 	want := fastUrl
-
 	got := Racer(slowUrl, fastUrl)
 
 	if got != want {
 		t.Errorf("got %q but want %q", got, want)
 	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	return server
 }
